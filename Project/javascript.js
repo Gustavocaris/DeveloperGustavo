4045d659b127b0b8a5a17f21de71d95fe75509fa
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-07-08 20:06:39 -0300

    aplicando responsividade

--- Project/javascript.js
+++ Project/javascript.js
@@ -1,3 +1,18 @@
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('.navbar');
+const links = document.querySelectorAll('.navbar-links li a');
+
+links.forEach(item => {
+  item.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
+
+menuHamburguer.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
