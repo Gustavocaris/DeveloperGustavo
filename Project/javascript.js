9f9de27fc7fc3639cc5d41ef785f14ce770edc69
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-07-20 17:18:43 -0300

    Responsividade no JS

--- Project/javascript.js
+++ Project/javascript.js
@@ -1,38 +1,78 @@
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 const nav = document.querySelector('.navbar');
 const links = document.querySelectorAll('.navbar-links li a');
+let header = document.getElementById('header');
+let swiper;
 
-links.forEach(item => {
-  item.addEventListener('click', () => {
+// Verifique se os elementos existem antes de adicionar event listeners
+if (menuHamburguer && nav) {
+  menuHamburguer.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
-});
+}
 
-menuHamburguer.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
+if (links) {
+  links.forEach(item => {
+    item.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  });
+}
 
 
-
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+function createSwiper(container, pagination, nextButton, prevButton) {
+  return new Swiper(container, {
+    slidesPerView: handlewidth(),
     spaceBetween: 30,
     pagination: {
-      el: ".swiper-pagination",
+      el: pagination,
       clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+      nextEl: nextButton,
+      prevEl: prevButton,
+    },
   });
+}
 
-let header = document.getElementById('header');
+function handlewidth() {
+  let getwidth = window.innerWidth || document.documentElement.clientWidth;
+  let slideshow = 3;
 
+  if (getwidth < 1001) {
+    slideshow = 2;
+  }
+
+  if (getwidth < 700) {
+    slideshow = 1;
+  }
+
+  return slideshow;
+}
+
+swiper = createSwiper(
+  ".mySwiper",
+  ".swiper-pagination",
+  ".swiper-button-next",
+  ".swiper-button-prev"
+);
+
+
+
+// Atualize o Swiper quando a janela for redimensionada
+window.addEventListener('resize', () => {
+  swiper.params.slidesPerView = handlewidth();
+  swiper.update();
+});
+
+
+// Altere o estilo de fundo do cabeçalho ao rolar a página
 window.addEventListener('scroll', () => {
+  if (header) {
     if (window.scrollY >= 200) {
       header.style.background = '#191919';
     } else {
       header.style.background = 'transparent';
     }
-  });
+  }
+});
